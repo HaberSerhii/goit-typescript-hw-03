719ab71309fa5c18a04a63a837173e416c3fea56
Serhii Haber <haberserhii@icloud.com>
2023-11-28 16:48:53 +0200

    Update 2.ts

--- src/2.ts
+++ src/2.ts
@@ -15,24 +15,17 @@
 */
 
 class Employee {
-  // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
-
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
+  constructor(public name: string, private department: string, protected salary: number) {  
   }
-
   getEmployeeDetails() {
     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
   }
 }
 
 class Manager extends Employee {
-  // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string,salary:number ) {
+    super(name, department, salary + 1000);
+  }
 }
 
 
